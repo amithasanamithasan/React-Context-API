0631af453e08b35b096135688dbfe3b3b15f6021
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-08-29 15:04:13 +0600

    Multiply using Context Api

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,26 @@ import './App.css'
 import ChieldComponent from './Components/ChildComponent/ChieldComponent'
 
  export const CountContext= createContext();
+export const MultiplyContext = createContext();
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [multiply, setMultiply] = useState(1)
   return (
     <>
    
- <CountContext.Provider value={count}>
+<MultiplyContext.Provider value={multiply}>
+<CountContext.Provider value={count}>
  <button className=' border-t-cyan-600  bg-orange-400 rounded-2xl text-3xl font-serif ' onClick={() => setCount((count) => count + 1)}>
           count is- {count}
         </button>
-  
+        <button className=' border-t-cyan-600  bg-orange-400 rounded-2xl text-3xl font-serif ' onClick={() => setMultiply((multiply) => multiply * 2)}>
+          Multiply is- {multiply}
+        </button>
         <ChieldComponent></ChieldComponent>
       
  </CountContext.Provider>
+</MultiplyContext.Provider>
       
     
     </>
--- src/Components/AnotherChild/AnotherChildComponet.jsx
+++ src/Components/AnotherChild/AnotherChildComponet.jsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import GrandChild from "../GrandChid/GrandChild";
+import { MultiplyContext } from "../../App";
 
 
 const AnotherChildComponet = () => {
+ const Multiply=   useContext(MultiplyContext)
     return (
         <div>
-            <button className="bg-lime-400 text-3xl font-serif rounded-xl" type="button" >AnotherChild-</button>
+            <button className="bg-lime-400 text-3xl font-serif rounded-xl" type="button" >AnotherChild-{Multiply}</button>
  
             <GrandChild>s</GrandChild>
         </div>
