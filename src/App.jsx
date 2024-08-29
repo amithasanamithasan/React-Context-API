a214ed306cfbda19893997c73fbd989c54b15cab
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-08-29 14:55:03 +0600

    Using Context Api app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 
 import './App.css'
 import ChieldComponent from './Components/ChildComponent/ChieldComponent'
 
-
+ export const CountContext= createContext();
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,12 +11,14 @@ function App() {
   return (
     <>
    
-        <button className=' border-t-cyan-600  bg-orange-400 rounded-2xl text-3xl font-serif ' onClick={() => setCount((count) => count + 1)}>
+ <CountContext.Provider value={count}>
+ <button className=' border-t-cyan-600  bg-orange-400 rounded-2xl text-3xl font-serif ' onClick={() => setCount((count) => count + 1)}>
           count is- {count}
         </button>
   
         <ChieldComponent></ChieldComponent>
       
+ </CountContext.Provider>
       
     
     </>
--- src/Components/GrandChid/GrandChild.jsx
+++ src/Components/GrandChid/GrandChild.jsx
@@ -1,9 +1,12 @@
+import { useContext } from "react";
+import { CountContext } from "../../App";
 
 
 const GrandChild = () => {
+    const Count =useContext(CountContext)
     return (
         <div>
-            <button className="bg-blue-500 tex-3xl font-serif rounded-full "  type="button">GrandChid-</button>
+            <button className="bg-blue-500 text-3xl font-serif rounded-full "  type="button">GrandChid-{Count}</button>
         </div>
     );
 };
